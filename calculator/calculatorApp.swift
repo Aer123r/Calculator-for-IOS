1959a685e4e616b6a991617874bdc48976e3fa82
Aer123r <1834237464@qq.com>
2023-09-22 19:06:20 +0800

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by WuGaoyuan on 2023/9/22.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
